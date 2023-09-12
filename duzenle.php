1cb212c93a44c703dde84cf1f5d5da925f98f4f6
furkan-ozgul <118172591+furkan-ozgul@users.noreply.github.com>
2023-09-12 10:34:52 +0300

    Update duzenle.php

--- duzenle.php
+++ duzenle.php
@@ -6,7 +6,7 @@ include("vt.php"); // veritabanı bağlantımızı sayfamıza ekliyoruz.
 <html>
 <head>
 <meta charset="utf-8">
-<title>Veritabanı İşlemleri</title>
+<title>Veritabanı İşlemleriiiiii</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <header style="background-color: #990000">
         <img src="logo.png" width="100" style="border-radius: 50%;margin-left: 25%">
